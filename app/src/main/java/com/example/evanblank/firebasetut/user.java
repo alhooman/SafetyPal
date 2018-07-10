d9b84aabf630f21a4d503376c43c038d2a5ef3c7
ali <ali@eduroam-169-233-243-171.ucsc.edu>
2018-07-10 15:59:49 -0700

    Organized files

--- app/src/main/java/com/example/evanblank/firebasetut/user.java
+++
@@ -1,8 +0,0 @@
-package com.example.evanblank.firebasetut;
-public class user {
-    public String name;
-
-    public user(String user){
-        this.name = user;
-    }
-}
--- app/src/main/java/user.java
+++
@@ -1,7 +0,0 @@
-public class user {
-    public String name;
-
-    public user(String user){
-        this.name = user;
-    }
-}
