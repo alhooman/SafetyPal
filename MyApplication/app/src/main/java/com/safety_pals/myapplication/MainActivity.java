d948fcfadbcbb6d8e21cf91f7d82a4d3f573c17b
SusanaMargaritaEsparza <smesparz@ucsc.edu>
2018-07-18 22:58:56 -0700

    Edit homepage and contact pages on the UI

--- MyApplication/app/src/main/java/com/safety_pals/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/safety_pals/myapplication/MainActivity.java
@@ -4,17 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+//function to add contacts and color
 
 public class MainActivity extends AppCompatActivity {
+    String items[] = new String[]{"A", "B", "C", "D"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button);
+        ListView listView = (ListView) findViewById(R.id.ContactListButton);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
 }
